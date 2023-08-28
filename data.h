d835493cf086013571a454f9c74c5df10249ce6a
Jrizzo123 <77479285+Jrizzo123@users.noreply.github.com>
2023-08-28 15:58:59 -0500

    Copying file over

---
+++ data.h
@@ -0,0 +1,23 @@
+/*
+* Name: Jacob Rizzo
+* Final project
+* Purpose: data header file
+*/
+
+#ifndef DATA_H
+#define DATA_H
+
+#include <iostream>
+#include <vector>
+using std::string;
+using std::vector;
+
+
+
+struct Node {
+	int id;
+};
+
+
+
+#endif
