420adb1d540152d8c964e48b0004e3b46e932b5d
Jrizzo123 <77479285+Jrizzo123@users.noreply.github.com>
2023-08-28 16:01:13 -0500

    copying over graph interface

---
+++ graph.h
@@ -0,0 +1,47 @@
+/*
+* Name: Jacob Rizzo
+* Final project
+* Purpose: Interface for graph ADT
+*/
+
+#ifndef CLASSES_GRAPH_H
+#define CLASSES_GRAPH_H
+
+#include "data.h"
+#include <iostream>
+#include <iomanip>
+#include <queue>
+using std::cout;
+using std::endl;
+
+class Graph {
+public:
+	Graph();
+	~Graph();
+
+	bool addVertex(int, int, int);
+	bool addEdge(int, int, int);
+	bool deleteEdge(int, int);
+	bool removeVertex(int);
+	void printMatrix();
+	bool isEmpty();
+	int getEdgeCount();
+	int getVertexCount();
+	bool isConnected(int, int);
+	void clearGraph();
+	void breadthFirstSearch(int);
+	void depthFirstSearch(int);
+
+private:
+	vector<Node*> nodeList;
+	vector<vector<int>> adjMatrix;
+	int edgeCount, vertexCount;
+	int findIndex(int);
+	void resize(size_t);
+	bool find(int, vector<int>);
+	void depthFirstSearch(int, vector<bool>&);
+};
+
+
+
+#endif
